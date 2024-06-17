284854120d21fc4c4c13f3baeefc0022229bc995
manojbhatta500 <manojbhatta500@gmail.com>
2024-06-17 21:28:51 +0545

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
+/*
+1.create branch and work on your branch 
+2. don't work on main branch 
+3.don't use too much package (bloc equatable and other necessary package)
+
+we will use feature folder structure 
+-bloc
+-repository 
+-pages
+-widgets 
+-utils
+
+
+for now just this 
+
+*/
